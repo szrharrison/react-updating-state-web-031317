cdb7f08db220886a7d4023135050951cd3afc2a3
szrharrison <s.z.r.harrison@gmail.com>
2017-05-16 13:01:59 -0400

    Done.

---
+++ test/index-test.js
@@ -0,0 +1,10 @@
+describe('Tests', function () {
+
+  it(`shouldn't be a part of this readme lesson.`, function () {
+    expect(true).toEqual(true);
+  });
+
+  it('should exist since this lesson expects there to be tests for some reason.', function () {
+    expect(true).toEqual(true);
+  });
+});
